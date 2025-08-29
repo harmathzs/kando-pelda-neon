0c6c9242594be2d093120f6cdd98898290f93d09
Zsolt Harmath <harmathzs@gmail.com>
2025-08-29 14:57:06 +0200

    Add records and query

--- react-app/netlify/functions/neon-db.js
+++ react-app/netlify/functions/neon-db.js
@@ -1,8 +1,15 @@
+import { neon } from '@netlify/neon';
+
 export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello Neon-DB!',
-    }),
-  }
+    const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+    const catId = 1;
+    const [post] = await sql`SELECT * FROM cats WHERE id = ${catId}`;
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello Neon-DB!',
+            'QueryResult': post,
+        }),
+    }
 }
